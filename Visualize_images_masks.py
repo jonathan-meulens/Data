8046f1962dd203ebd973ccd069730ab9f19dbc35
jonathan-meulens <120602760+jonathan-meulens@users.noreply.github.com>
2024-05-16 16:24:40 +0200

    Visualize_images_masks.py

--- Visualize_images_masks.py
+++ Visualize_images_masks.py
@@ -1,74 +1,50 @@
-import torch
+#Import Libraries
 import os
 from glob import glob
-from model import UNET
-from tqdm import tqdm
 from dataset import MyOPSDataset
-from torch.utils.data import Dataset
 from monai.data import DataLoader
 from monai.utils import first
 import matplotlib.pyplot as plt
 from monai.transforms import (
-Compose,
-LoadImaged,
-ToTensord,
-AddChanneld,
-Orientationd,
-Spacingd,
-RandRotated,
-ScaleIntensityRanged,
-RandGaussianNoised,
-RandAffined,
-Flipd,
-CropForegroundd,
+    Compose,
+    LoadImaged,
+    ToTensord,
+    AddChanneld,
+    Orientationd,
+    Spacingd,
+    RandRotated,
+    ScaleIntensityRanged,
+    RandGaussianNoised,
+    RandAffined,
+    Flipd,
+    CropForegroundd,
 )
 
-LEARNING_RATE = 1e-4
-BATCH_SIZE = 1
-NUM_EPOCHS = 5
-NUM_WORKERS = 2
-IMAGE_HEIGHT = 482
-IMAGE_WIDTH = 478
-DEPTH= 5
-PIN_MEMORY = True
-LOAD_MODEL = True
 
+#Training images folder
 TRAIN_DIR = 'C:\\Users\\victo\\anaconda3\\envs\\Jay\\Data\\MyoSeg\\train25\\'
-TRAIN_DIR_2 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\'
+#Mask images folder 
 TRAIN_MASKDIR = 'C:\\Users\\victo\\anaconda3\\envs\\Jay\\Data\\MyoSeg\\train25_myops_gd\\'
-TEST_DIR_1 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\test_model1\\'
-TEST_DIR_2 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\test20\\'
 
-# The different TRAINING-SET folds for five-fold cross validation: #
-FOLD_1 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\train_fold_1\\'
-FOLD_2 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\train_fold_2\\'
-FOLD_3 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\train_fold_3\\'
-FOLD_4 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\train_fold_4\\'
-VAL_5 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\val_fold_5\\'
 
-# The different MASK-SET folds for five-fold cross validation: #
-MASK_FOLD_1 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\mask_model1\\mask_fold_1\\'
-MASK_FOLD_2 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\mask_model1\\mask_fold_2\\'
-MASK_FOLD_3 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\mask_model1\\mask_fold_3\\'
-MASK_FOLD_4 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\mask_model1\\mask_fold_4\\'
-MASK_VAL_5 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\mask_model1\\mask_val_fold_5\\'
-
-# The different MASK-SET folds for five-fold cross validation: #
-TEST_FOLD_1 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\test_model1\\test_fold_1\\'
-TEST_FOLD_2 = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\test_model1\\test_fold_1\\'
-
-SAVED_PREDS = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\predictions_model_1\\'
-MODEL1_CHEKCPOINTS = 'C:\\Users\\victo\\anaconda3\\envs\\JayM\\Jconda\\Data\\MyoSeg\\train_model1\\model1_checkpoints\\my_checkpoint.pt'
-
-#-------------------------LOADERS------------------------------------#
+#-------------------------If you want to see how the augmentations look like------------------------------------#
+#Main function
 def main():
+    #Folder where the images lie
     DATA_DIR = 'C:\\Users\\victo\\anaconda3\\envs\\Jay\\Data\\MyoSeg\\'
 
+    #Training images (NOTE: This FOLDER seperation is the same as the one we do below by accessing the 2 seperate folders)
+    3##(Its the same because the other 2 folders, are in DATA_DIR)
+    
     training_images = glob(os.path.join(DATA_DIR, 'train25'))
+
+    #Mask images
     mask_images = glob(os.path.join(DATA_DIR, 'train25_myops_gd'))
 
+    #Transform the the data in the dict in list format
     train_files = [{'images':images, 'masks':masks} for images,masks in zip(training_images, mask_images)]
 
+    #Add the augmentations
     original_transform = Compose(
         [
             LoadImaged(keys=["image", "label"])
@@ -88,6 +64,7 @@ def main():
             LoadImaged(keys=["image", "label"]),
         ])
 
+    #Create the Dataset and loader objects
     train_ds = MyOPSDataset(image_dir=TRAIN_DIR,
                             mask_dir=TRAIN_MASKDIR,
                             transform= val_transform
@@ -114,14 +91,15 @@ def main():
         shuffle=True,
         timeout=20)
 
+    #Grab the first augmented image
     test_patient = first(train_loader)
     test_image = test_patient['image']
-    #print(test_image.shape)
-
+    
+    #Grab the first normal (unchanged) image
     original_patient = first(original_loader)
     original_image = original_patient['image']
-    #print(original_image.shape)
 
+    #-----------------------Visualize the Augmentations-----------------------------#
     plt.figure('test', (12,6))
 
     #Original Image
@@ -139,10 +117,10 @@ def main():
     plt.title('mask patient')
     plt.imshow(test_patient['label'][0, 0, 0, : , :], cmap='gray')
 
+    #Show images
     plt.show()
 
-#if __name__ == '__main__':
-#    main()
+
 
 
 
