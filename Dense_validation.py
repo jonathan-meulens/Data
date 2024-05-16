17c78c18a9afe0dafea882a752c8eeb7e860201e
jonathan-meulens <120602760+jonathan-meulens@users.noreply.github.com>
2024-05-16 15:37:14 +0200

    Dense_validation.py

--- Dense_validation.py
+++ Dense_validation.py
@@ -1,18 +1,8 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from Dice_score import accuracy, dice_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.linear_model import LogisticRegression
-from dataset import MyOPSDataset
-from model import UNET
-import albumentations as A
-import torch.nn.functional as F
-from tqdm import tqdm
-import numpy as np
-from monai.data import Dataset, DataLoader
 
-
+#Validation for the Dense_UNET_model
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 NUM_EPOCHS = 10
 def val_dense(loader, model, x_axis):
@@ -27,15 +17,12 @@ def val_dense(loader, model, x_axis):
         x, y = batch
         x = x.to(device=DEVICE, dtype=torch.float32)
         y = y.to(device=DEVICE, dtype=torch.float32)
-        #print(y.shape)
         with torch.no_grad():
             preds = model(x)
-            #print(preds.shape)
             acy = accuracy(y,preds)
             # forward
             # with torch.cuda.amp.autocast():
             preds = torch.sigmoid(preds)
-            #print(y[(y>1)])
             y = torch.sigmoid(y)
             loss = loss_fn(preds, y)
             initial_loss += loss.item()
